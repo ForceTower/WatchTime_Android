62a960145103422697826244b6d038052f0e1b81
ForceTower <joaopaulo761@gmail.com>
2017-01-24 00:47:25 -0300

    Some life cycle memes

--- mobile/src/main/java/com/watchtime/fragments/NavigationDrawerFragment.java
+++ mobile/src/main/java/com/watchtime/fragments/NavigationDrawerFragment.java
@@ -1,5 +1,7 @@
 package com.watchtime.fragments;
 
+import android.app.Activity;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -7,6 +9,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
 import com.watchtime.adapters.NavigationAdapter;
+import com.watchtime.base.content.preferences.Prefs;
+import com.watchtime.base.utils.PrefUtils;
 
 /**
  * Created by João Paulo on 23/01/2017.
@@ -42,5 +46,41 @@ public class NavigationDrawerFragment extends Fragment implements NavigationAdap
     @Override
     public int getSelectedPosition() {
         return mCurrentSelectedPosition;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mCallbacks = (Callbacks) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException("Activity must implement NavigationDrawerCallbacks.");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mCallbacks = null;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Checks if the user knows how to use the drawer
+        mUserLearnedDrawer = PrefUtils.get(getActivity(), Prefs.DRAWER_LEARNED, false);
+
+        if (savedInstanceState != null) {
+            mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION); //Received Navigation position on creation Yeee booy
+            mFromSavedInstanceState = true;
+        }
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // Indicate that this fragment would like to influence the set of actions in the action bar.
+        setHasOptionsMenu(true);
     }
 }
